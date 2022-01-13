7baad53d1270a4d906f31c7563b9900f4895bf5b
ChriEffe <christianferrazzano93@gmail.com>
2022-01-13 15:57:06 +0100

    header init

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <header class="container">
+        <img src="../assets/img/dc-logo.png" alt="">
+        <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </header>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+header  {
+    display: flex;
+    justify-content: space-between;
+    ul  {
+        list-style: none;
+        display: flex;
+    }
+}
+</style>
