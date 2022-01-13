bafb00195481b4bbdd0dfacc1786141f04a760b5
ChriEffe <christianferrazzano93@gmail.com>
2022-01-13 18:03:34 +0100

    main

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,22 +2,29 @@
     <header class="container">
         <img src="../assets/img/dc-logo.png" alt="">
         <ul>
-            <li><a href="">characters</a></li>
+            <li><a href="#">characters</a></li>
             <li class="active"><a href="">comics</a></li>
-            <li><a href="">movies</a></li>
-            <li><a href="">tv</a></li>
-            <li><a href="">games</a></li>
-            <li><a href="">collectibles</a></li>
-            <li><a href="">videos</a></li>
-            <li><a href="">fans</a></li>
-            <li><a href="">news</a></li>
-            <li><a href="">shop</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
         </ul>
     </header>
 </template>
 
 <script>
-
+export default {
+    name: 'Header',
+    data(){
+        return {
+            title: "Lorem Ipsum"
+        }
+    }
+}
 </script>
 
 <style lang="scss" scoped>
---
+++ src/components/Main.vue
@@ -0,0 +1,83 @@
+<template>
+    <main>
+        <div class="main-top">
+            <div class="container">
+                <h1>--> Content Goes Here</h1>
+            </div>
+        </div>
+        <div class="main-bottom">
+            <div class="container">
+                <div class="content">
+                    <img src="../assets/img/buy-comics-digital-comics.png" alt="">
+                    <p class="caption">digital comics</p>
+                </div>
+                <div class="content">
+                    <img src="../assets/img/buy-comics-merchandise.png" alt="">
+                    <p class="caption">dc merchandise</p>
+                </div>
+                <div class="content">
+                    <img src="../assets/img/buy-comics-subscriptions.png" alt="">
+                    <p class="caption">subscriptions</p>
+                </div>
+                <div class="content">
+                    <img src="../assets/img/buy-comics-shop-locator.png" alt="">
+                    <p class="caption">dc shop locator</p>
+                </div>
+                <div class="content">
+                    <img src="../assets/img/buy-dc-power-visa.svg" alt="">
+                    <p class="caption">dc power visa</p>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'Main',
+    data(){
+        return {
+            title: "Lorem Ipsum"
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+    @import "../assets/scss/style.scss";
+    .main-top{
+        height: 150px;
+        display: flex;
+        align-items: flex-start;
+        background-color: #1c1c1c;
+        }
+        h1 {
+            color: white;
+        }
+        .container {
+            display: flex;
+            justify-content: start;
+            align-items: center;
+            height: 100%;
+        }
+        .main-bottom {
+            width: 100%;
+            height: 150px;
+            background-color: #0282f9;
+            text-transform: uppercase;
+        }
+        .content {
+            display: flex;
+            justify-content: center;
+            width: calc(100% / 5);
+            align-items: center;
+            gap: 10px;
+            img{
+            width: 20%;
+            }
+            .caption {
+                font-size: 0.8em;
+                color: white;
+            }   
+        }
+</style>
