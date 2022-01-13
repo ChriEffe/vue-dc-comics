7b0ed13879173540540743994f28ae993367bad2
ChriEffe <christianferrazzano93@gmail.com>
2022-01-13 17:00:55 +0100

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,16 +2,16 @@
     <header class="container">
         <img src="../assets/img/dc-logo.png" alt="">
         <ul>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
+            <li><a href="">characters</a></li>
+            <li class="active"><a href="">comics</a></li>
+            <li><a href="">movies</a></li>
+            <li><a href="">tv</a></li>
+            <li><a href="">games</a></li>
+            <li><a href="">collectibles</a></li>
+            <li><a href="">videos</a></li>
+            <li><a href="">fans</a></li>
+            <li><a href="">news</a></li>
+            <li><a href="">shop</a></li>
         </ul>
     </header>
 </template>
@@ -22,11 +22,32 @@
 
 <style lang="scss" scoped>
 header  {
+    height: 150px;
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    padding: 10px 0;
     ul  {
         list-style: none;
         display: flex;
+        color: black;
+        font-weight: bold;
+        text-transform: uppercase;
+        li  {
+            padding: 0 0.5em;
+            a  {
+                text-decoration: none;
+                color: black;
+                padding: 64px 0;
+                cursor: pointer;
+                &:hover,
+                &:active,
+                &.active  {
+                    color: #0282f9;
+                    border-bottom: 3px solid #0282f9;
+                }
+            }
+        } 
     }
 }
 </style>
